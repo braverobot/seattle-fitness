aee343779ece8b85db7f49ff20ac0bffa27227bc
Brian Heartwood <briheartwood@gmail.com>
2023-05-20 11:56:20 -0700

    Adding jinja pages for studios

--- app.py
+++ app.py
@@ -25,14 +25,17 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
 
 
-# Routes
-# have homepage route to /seattle-fitness by default for convenience
+#
+# Routes for Home Page (index.html that sits in static folder)
+# -----------------------------------------------------------------------------------
 @app.route("/")
 def home():
     return send_from_directory('static', 'index.html')
 
 
-# route for Customers page
+#
+# Routes for Customers page
+# -----------------------------------------------------------------------------------
 @app.route("/customers", methods=["POST", "GET"])
 def customers():
     """This is to render the customers page to display them from the DB"""
@@ -65,8 +68,6 @@ def customers():
         return redirect("/customers")
 
 
-# This is the route for delete customers functionality. We want to pass the
-# 'customer_id' value of that person on button click (see HTML) via the route
 @app.route("/delete_customer/<int:customer_id>")
 def delete_customer(customer_id):
     # mySQL query to delete the person with our passed id
@@ -78,8 +79,7 @@ def delete_customer(customer_id):
     # redirect back to Customers page after the action is taken
     return redirect("/customers")
 
-# This is the route to update a customer's information. We want to pass the
-# 'customer_id' value of that person on button click (see HTML) via the route
+
 @app.route("/update_customer/<int:customer_id>", methods=["POST", "GET"])
 def update_customer(customer_id):
     # Grab customer data so we send it to our template to display
@@ -110,12 +110,82 @@ def update_customer(customer_id):
         return redirect("/customers")
 
 
-@app.route("/studios", methods=["GET"])
+#
+# Routes for Studios page
+# -----------------------------------------------------------------------------------
+@app.route("/studios", methods=["POST", "GET"])
 def studios():
+    """This is to render the studios page to display them from the DB"""
+
+    # Grab studio data so we send it to our template to display
     if request.method == "GET":
-        return render_template("studios.j2")
-    # TODO (Brian): Add POST method for adding a studio and format the
-    # .j2 template better
+        # mySQL query to grab all the Studios in the Studios table
+        query = "SELECT studio_id, location, phone_number\
+                 FROM Studios"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        cur.close()
+        return render_template("studios.j2", data=data)
+
+    # Adding a studio using the POST method
+    if request.method == "POST":
+        location = request.form["location"]
+        phone_number = request.form["phone_number"]
+
+        # mySQL query to add a studio to the Studios table
+        query = "INSERT INTO Studios (location, phone_number) \
+                 VALUES (%s, %s);"
+        cur = mysql.connection.cursor()
+        cur.execute(query, (location, phone_number))
+        mysql.connection.commit()
+        cur.close()
+        # redirect back to Studios page
+        return redirect("/studios")
+
+
+@app.route("/delete_studio/<int:studio_id>")
+def delete_studio(studio_id):
+    # mySQL query to delete the person with our passed id
+    query = "DELETE FROM Studios WHERE studio_id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (studio_id,))
+    mysql.connection.commit()
+    cur.close()
+    # redirect back to Customers page after the action is taken
+    return redirect("/studios")
+
+
+@app.route("/update_studio/<int:studio_id>", methods=["POST", "GET"])
+def update_studio(studio_id):
+    # Grab studio data so we send it to our template to display
+    if request.method == "GET":
+        # mySQL query to grab all the studios in the Ctudios table
+        query = "SELECT studio_id, location, phone_number \
+                 FROM Studios WHERE studio_id = '%s';"
+        cur = mysql.connection.cursor()
+        cur.execute(query, (studio_id,))
+        data = cur.fetchall()
+        cur.close()
+        return render_template("update_studio.j2", data=data)
+
+    # Adding a studio using the POST method
+    if request.method == "POST":
+        location = request.form["location"]
+        phone_number = request.form["phone_number"]
+
+        # mySQL query to add a studio to the Studios table
+        query = "UPDATE Studios SET location = %s, phone_number = %s, \
+                 WHERE studio_id = %s;"
+        cur = mysql.connection.cursor()
+        cur.execute(query, (location, phone_number, studio_id))
+        mysql.connection.commit()
+        cur.close()
+        # redirect back to Studios page
+        return redirect("/studios")
+
+
+
 
 # Listener
 # Run python app.py to run locally and then browse to http://localhost:8000
